6e0d07e418b1ef665cdb6cf809c21084557c5341
Gustavo Souza <gustavodsouzaaa@gmail.com>
2023-06-28 20:34:02 -0300

    botão, nome das pessoas no app e alteração de theme

--- app/src/main/java/com/example/barbershop/view/Home.kt
+++ app/src/main/java/com/example/barbershop/view/Home.kt
@@ -7,18 +7,26 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import com.example.barbershop.R
+import com.example.barbershop.databinding.ActivityHomeBinding
 import com.example.barbershop.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class Home : AppCompatActivity() {
 
-    class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
+            binding = ActivityHomeBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            supportActionBar?.hide()
+
+            val nome = intent.extras?.getString("nome")
+
+            binding.txtNomeUsuario.text = "Bem-Vindo(a), $nome"
         }
     }
 
-    }
+
 
 
