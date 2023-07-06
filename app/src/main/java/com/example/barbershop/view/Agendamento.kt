7667401c2527a8f1c2caafb9beee8ebc08ad199f
Gustavo Souza <gustavodsouzaaa@gmail.com>
2023-07-05 22:48:53 -0300

    agendamento

--- app/src/main/java/com/example/barbershop/view/Agendamento.kt
+++ app/src/main/java/com/example/barbershop/view/Agendamento.kt
@@ -1,14 +1,21 @@
 package com.example.barbershop.view
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import com.example.barbershop.R
 import com.example.barbershop.databinding.ActivityAgendamentoBinding
+import java.util.Calendar
 
 class Agendamento : AppCompatActivity() {
 
     private lateinit var binding: ActivityAgendamentoBinding
+    private val calendar: Calendar = Calendar.getInstance()
+    private var data: String = ""
+    private var hora: String = ""
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAgendamentoBinding.inflate(layoutInflater)
@@ -19,7 +26,21 @@ class Agendamento : AppCompatActivity() {
         val nome = intent.extras?.getString("nome").toString()
 
         val datePicker = binding.datePicker
+        datePicker.setOnDateChangedListener { _, year, monthOfYear, dayOfMonth ->
+            
 
+            calendar.set(Calendar.YEAR,year)
+            calendar.set(Calendar.MONTH,monthOfYear)
+            calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth) 
+            
+            var dia = dayOfMonth.toString()
+            val mes: String
+            
+            
+            
+            
+            
+        }
 
     }
 }
